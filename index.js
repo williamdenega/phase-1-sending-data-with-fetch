dcfb3cfd92e49aa0242a415dc98a1d9030e20aa7
WilliamDenega <williamdenega@gmail.com>
2021-12-20 21:01:14 -0500

    commit tree 7321eda82bbde6acf80d1a1e97fbf5f310f5239b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(name ,email){
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        })
+    })
+    .then(res => res.json())
+    .then(user => document.body.innerHTML = user[ 'id'])
+    .catch(error => document.body.innerHTML = error.message)
+}
+
